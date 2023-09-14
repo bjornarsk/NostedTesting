d79ed5dba67e7402859bb2d26bb7733d80cbcbe0
Bjørnar Hatling Skåden <bjornarhatlingskaden@Bjrnar-sin-Air.uia.no>
2023-09-14 13:45:07 +0200

    hei

---
+++ testgithub/testgithub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace testgithub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
